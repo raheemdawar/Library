773e82e053d01d3977b281a94d7c61aef98eb2e8
Raheem <raheemdawar89@gmail.com>
2018-05-08 17:44:38 +0500

    V1

---
+++ LibraryProject/LibraryProject/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryProject.Models
+{
+    public class Student
+    {
+        public string student_Name { get; set; }
+        public string student_CellNo { get; set; }
+        public string student_DOB { get; set; }
+        public string student_isActive { get; set; }
+        public string student_CreateDate { get; set; }
+        public string student_UpdateDate { get; set; }
+      
+
+    }
+}
